38ce59936217220d032b6a8b0f9e0f3e86b61728
LS <song-liu@ceyes.cn>
2016-02-23 11:26:58 +0800

    add the mongodb support

---
+++ clean-app-with-db.sh
@@ -0,0 +1,8 @@
+oc project mytest
+oc delete service nodejs-mongodb-example
+oc delete service mongodb
+oc delete route nodejs-mongodb-example
+oc delete is nodejs-mongodb-example
+oc delete bc nodejs-mongodb-example
+oc delete dc nodejs-mongodb-example
+oc delete dc mongodb
